b887839d5d225574841f691e8a0a4c95580ff143
thiagolosalmeidas <thyagolosalmeidas@hotmail.com>
2023-09-10 17:42:43 -0300

    add desafio conta-terminal

--- desafios/conta-banco/ContaBanco/src/ContaTerminal.java
+++ desafios/conta-banco/ContaBanco/src/ContaTerminal.java
@@ -9,26 +9,28 @@ public class ContaTerminal {
         //TODO: Exibir as mensagens para nosso usuário
         
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-        
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
-
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
 
         //TODO: obter pela classe Scanner os valores digitados no terminal.
 
+        System.out.println("Digite seu Nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o Número de sua conta");
+        int conta = scanner.nextInt();
+        
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
 
         //TODO: exibir a mensagem da conta criada.
+        System.out.println("#############################");
+        System.out.println("Olá, " + nomeCliente + ".");
+        System.out.println("Obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua Agência é: " + agencia + "," + " " + "conta: " + conta + ".");
+        System.out.println("E seu saldo de: " + "R$ " + saldo + " já está disponível para saque.");
+        System.out.println("Tenha um bom dia!");
 
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
     }
 }
