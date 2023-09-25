d73f94c60ab2e53c801ee9ee4dea689f65390d0a
thiagolosalmeidas <thyagolosalmeidas@hotmail.com>
2023-09-24 23:18:56 -0300

    Organizando Seus Ativos

--- controle-fluxo/estruturas-condicionais/ControleFluxo/src/ExemploForArray.java
+++ controle-fluxo/estruturas-condicionais/ControleFluxo/src/ExemploForArray.java
@@ -1,7 +1,7 @@
 // ExemploFor.java
 public class ExemploForArray {
 public static void main(String[] args) {
-	String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+	String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS","AUGUSTO" };
 
 //	for (int x=0; x<alunos.length; x++) {
 //		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
